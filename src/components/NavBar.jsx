13730972dde32de42d394e132c6bdb78967a35b0
Aslan <aslan.mammadov96@gmail.com>
2024-05-25 19:48:34 -0400

    dark mode adjustment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import logo from "../assets/cube_logo.png";
-import {
-  HStack,
-  Image,
- 
-  Switch,
-  useColorMode,
-} from "@chakra-ui/react";
+import { HStack, Image, Switch, useColorMode, Text } from "@chakra-ui/react";
 import SearchBox from "./SearchBox";
 
-
 const NavBar = () => {
-    const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
-      <Image src={logo} boxSize="55px"/>
-      <SearchBox/>
-      <Switch paddingX='10px' onChange={toggleColorMode}/>
-    </HStack >
+      <Image src={logo} boxSize="55px" />
+      <SearchBox />
+      <Switch paddingX="10px" onChange={toggleColorMode} isChecked={colorMode==='dark'}/>
+      <Text whiteSpace='nowrap'>Dark Mode</Text>
+    </HStack>
   );
 };
 
