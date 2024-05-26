4680a6042bd4d5e5f44592416820977e172af392
Aslan <aslan.mammadov96@gmail.com>
2024-05-26 11:52:14 -0400

    style genre list

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import axios from 'axios'
-import apiClient from '../services/apiClient'
-import { List, ListItem } from '@chakra-ui/react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import apiClient from "../services/apiClient";
+import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 
 const Genres = () => {
-    const [genres, setGenres]=useState([])
-    const [error, setError]=useState([])
-    useEffect(()=>{
-      axios
-      .get(apiClient('/genres'))
-      .then(res=>setGenres(res.data.results))
-      .catch(err=>setError(err.message))
-    },[])
+  const [genres, setGenres] = useState([]);
+  const [error, setError] = useState([]);
+  useEffect(() => {
+    axios
+      .get(apiClient("/genres"))
+      .then((res) => setGenres(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
   return (
     <List>
-        {genres.map(genre=><ListItem key={genre.id}>
+      {genres.map((genre) => (
+        <ListItem key={genre.id}>
+          <HStack marginY='10px'>
+          <Image src={genre.image_background} boxSize='32px' borderRadius='12px'/>
+          <Button
+          variant={'link'}
+          >
             {genre.name}
-        </ListItem>)}
-      
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
     </List>
-  )
-}
+  );
+};
 
-export default Genres
+export default Genres;
